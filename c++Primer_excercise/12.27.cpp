caa6de22890e1d380c01cb9f0bebff7590e1a0cf
zhengjunhao <changchunjunhao@gmail.com>
2020-11-15 16:35:44 +0800

    Fix 12.27.cpp lines in result is empty issue.
    Cannot find out what it does.
    
    getline(text)   vs  string(pbuffer, len) ? which pBuffer will be free
    after that(unique_ptr)

--- c++Primer_excercise/12.27.cpp
+++ c++Primer_excercise/12.27.cpp
@@ -16,7 +16,7 @@ public:
         int count;
         string line;
     };
-    QueryResult(int occurTime) : occurTime(occurTime) {}
+    QueryResult(string query, int occurTime) : occurTime(occurTime), query(query) {}
     void append(int count, string line) {
         Pair p;
         p.count = count;
@@ -25,7 +25,7 @@ public:
     }
     string toString() {
         stringstream ss;
-        ss << "element occurs " << occurTime << " times" << endl;
+        ss << "element \"" << query << "\" occurs " << occurTime << " times" << endl;
         for (auto it = data.begin(); it != data.end(); it++) {
             ss << "Line " << it->count << ": " << "\"" << it->line << "\"" << endl;
         }
@@ -34,42 +34,28 @@ public:
 
 private:
     int occurTime;
+    string query;
     vector<Pair> data;
 };
 
 class TextQuery {
 public:
     TextQuery(ifstream& infile) {
-        filebuf* fbuf = infile.rdbuf();
-        int size = fbuf->pubseekoff(0, infile.end, infile.in);
-        fbuf->pubseekpos(0, infile.in);
 
-        unique_ptr<char[]> content(new char[size]);
-        fbuf->sgetn(content.get(), size);
-        fbuf->pubseekpos(0, infile.in);
-
-        // divide into lines
-        const char* ptr = content.get();
-        int index = 0, begin = 0;
-        while (index < size) {
-            begin = index;
-            while(index < size && content[index] != '\n') index++;
-            lines.push_back(string(ptr + begin, index-begin));
-            index += 1; // skip '\n', and it should work fine in the EOF
-        }
-
-        // Pre-Parse
-        string word;
-        int lineNumber = 1;
-        for (auto it = lines.begin(); it != lines.end(); it++, lineNumber++) {
-            istringstream is(*it);
+        string text, word;
+        while (getline(infile, text)) {
+            lines.push_back(text);
+            istringstream is(text);
+            int lineNumber = lines.size() - 1;
             while (is >> word) {
                 markOccurTime(word);
                 markLine(word, lineNumber);
                 cout << "mark line: word " << word << " lineNumber " << lineNumber << endl;
             }
         }
+    }
 
+    void test() {
         // test:
         for (int i = 0; i < lines.size(); i++) {
             cout << "Line " << i << ": " << lines[i] << endl;
@@ -77,7 +63,7 @@ public:
     }
     QueryResult query(string q) {
         int occurTime = getOccurTime(q);
-        QueryResult result(occurTime);
+        QueryResult result(q, occurTime);
         if (occurTime > 0) {
             // Add lines detail to report.
             getLineDetails(result, q);
@@ -130,6 +116,8 @@ ostream& print(ostream& out, QueryResult qr) {
 
 void runQueries(ifstream &infile) {
     TextQuery tq(infile);
+    tq.test();
+
     stringstream instruction;
     instruction << "We" << endl;
     instruction << "are" << endl;
